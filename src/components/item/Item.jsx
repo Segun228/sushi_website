930fe7d582421f7227d17843b16331d69a4faffe
Segun228 <segunperkele@gmail.com>
2025-04-09 09:17:48 +0300

    created dumb item component

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,8 +1,17 @@
 import './item.css';
 
-const Item = () => {
+const Item = (props) => {
     return (
         <>
+        <div className="item__wrapper">
+          <img src={props.item.image} alt="roll" className="item__photo" />
+          <div className="item__title">{props.item.title}</div>
+          <div className="item__info">
+            <div className="item__price">{props.item.price}₽</div>
+            <div className="item__macros">{props.item.macros}</div>
+          </div>
+          <button className="buy__button">В корзину</button>
+        </div>
         </>
       );
 }
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,22 @@
 import Header from "../header/Header";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import Item from "../item/Item";
+import philadelfia from "./../../assets/images/philadelphia_1.png"
+import { title } from "framer-motion/client";
+const item ={
+    id:1,
+    image:philadelfia,
+    title:"Филадельфия ролл",
+    price:300,
+    macros:"205 г / 6 шт / 120 ккал ",
+}
 const Layout = () => {
+     
     return ( 
     <>
     <Header></Header>
+    <Item item={item}></Item>
     <Outlet></Outlet>
     
     </>);
