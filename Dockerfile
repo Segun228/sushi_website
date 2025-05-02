2cea3e318da40048b9f7060e594b28f31d85b860
Segun228 <segunperkele@gmail.com>
2025-05-02 19:47:40 +0300

    adding dockerfile

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+
+FROM node:22-alpine
+
+WORKDIR /app
+
+COPY package.json package-lock.json ./
+
+RUN npm install --legacy-peer-deps
+
+COPY . .
+
+EXPOSE 8080
+
+CMD ["npm", "run", "dev"]
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['crimson_dragon.onrender.com', 'localhost'], 
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 8080
+  }
 })
